fd5694126ac4ea86165373c44e385196d064eae0
CaptnClementine <131146976+CaptnClementine@users.noreply.github.com>
2024-03-15 19:57:08 +0300

    Add 'filter_results_diff_split' to keepTADs spleets with different sizes
    Пока оставила коменты, чтобы было проще читать код :)

--- tads_find_overlaps.py
+++ tads_find_overlaps.py
@@ -43,3 +43,43 @@ def find_overlaps(table1: pd.DataFrame, table2: pd.DataFrame, bins: int = 100_00
                                  (row1['start'], row1['end']), #tuple  или pandas.interval (?)
                                  (row2['start'], row2['end'])))
     return pd.DataFrame(overlaps, columns=['Region1', 'Region2', 'Table1_length', 'Table2_length', 'Table1_coordinates', 'Table2_coordinates'])
+
+
+def filter_results_diff_split(results: pd.DataFrame, epsilon: int = 200_000) -> pd.DataFrame:
+    """
+    Filters regions with different TAD sizes between the same Region1 values.
+
+    Args:
+        results (pd.DataFrame): Output DataFrame from 'find_overlaps' fuction. Must include specified columns:
+            - Region1 - chromosome number from first table
+            - Region2 - chromosome number from second table
+            - Table1_length - length for each TAD from first table
+            - Table2_length - length for each TAD from second table
+            - Table1_coordinates - coordinates for each TAD from first table
+            - Table2_coordinates - coordinates for each TAD from second table
+        epsilon (int, optional): Threshold for considering TAD size differences significant. Defaults to 200000.
+
+    Returns:
+        pd.DataFrame: DataFrame containing filtered regions with significant TAD size differences.
+    Example:
+        filtered_results = filter_results_diff_split(results)
+    """
+    filtered_regions = []
+    for region1 in results['Region1'].unique():
+        df_region1 = results[results['Region1'] == region1]
+        unique_lengths = df_region1['Table2_length'].unique()
+
+        if len(unique_lengths) == 1: #если одно значение - выкидываем его из результатов
+          continue
+
+        for length in unique_lengths:
+                min_length = np.min(df_region1['Table2_length'])
+                max_length = np.max(df_region1['Table2_length'])
+                if max_length - min_length > epsilon: # если разница между двумя тадами больше, чем epsilon - то это наши разноразмерные тады
+                    filtered_regions.extend(df_region1.to_dict('records'))
+                    break
+    filtered_regions = pd.DataFrame(filtered_regions)
+    #следующая строка - для проверки странных ТАДов (может они и есть шифты, а может для обратной таблицы это сплит)
+    index_not_unique_TAD = filtered_regions.groupby(by=['Region1', 'Table1_coordinates'])['Region2'].transform('count') != 1
+    filtered_regions = filtered_regions.loc[index_not_unique_TAD]
+    return filtered_regions
