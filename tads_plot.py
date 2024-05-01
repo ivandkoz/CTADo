58bebe04237d0f4b98937e0f223bc0cd837ccd11
Victoria Orlova <115810359+duckweedGuard@users.noreply.github.com>
2024-05-01 18:55:33 +0300

    Update tads_plot.py

--- tads_plot.py
+++ tads_plot.py
@@ -12,16 +12,22 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings(action='ignore', message='Mean of empty slice')
 pd.options.mode.chained_assignment = None
+warnings.filterwarnings("ignore")
 
 
 REPLACE_DICT = {'chr1': '1', 'chr2': '2', 'chr3': '3', 'chr4': '4', 'chr5': '5', 'chr6': '6', 'chr7': '7', 'chr8': '8',
                 'chr9': '9', 'chr10': '10', 'chr11': '11', 'chr12': '12', 'chr13': '13', 'chr14': '14', 'chr15': '15',
                 'chr16': '16', 'chr17': '17', 'chr18': '18', 'chr19': '19', 'chr20': '20', 'chr21': '21'}
 
-
-def plot_tads(tads_annot, boundaries_df_clr_filename):
+def plot_tads(tads_annot, boundaries_df_clr_filename, window):
     boundaries_df_clr = pd.read_csv(f'{boundaries_df_clr_filename}', index_col=0)
-    boundaries_df_clr = boundaries_df_clr.loc[boundaries_df_clr[f'is_boundary_{window}'] == True]
+    
+    if boundaries_df_clr.shape[1] != 3:
+        boundaries_df_clr = boundaries_df_clr.loc[boundaries_df_clr[f'is_boundary_{window}'] == True]
+        boundaries_df_clr = boundaries_df_clr.reset_index()
+        boundaries_df_clr.drop('index', axis= 1 , inplace= True)   
+        boundaries_df_clr = boundaries_df_clr[['chrom', 'start', 'end']]
+
     boundaries_df_clr['chrom'] = boundaries_df_clr['chrom'].replace(REPLACE_DICT)
     boundaries_df_clr = boundaries_df_clr.rename(columns={'chrom': 'Chromosome', 'start': 'Start', 'end': 'End'})
     boundaries_df_clr = boundaries_df_clr[['Chromosome', 'Start', 'End']]
@@ -30,28 +36,48 @@ def plot_tads(tads_annot, boundaries_df_clr_filename):
     return merged_clr
 
 
-def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundaries_df_clr2_filename, resolution, binsize, rslt_df_name):
-    rslt_df = pd.read_csv(f'{rslt_df_name}', index_col=0)
-    df = rslt_df.sort_values('difference', ascending=False).head(5)
-    most_diff_tads = []
-    binsize = binsize * 1.5
-    for index, row in df.iterrows():
-        most_diff_tads.append([row['chrom'],  max(row['start_1'], row['start_2']) - binsize * 5,
-                               max(row['end_1'], row['end_2']) + binsize * 5])
+# change: intensity or split/merge
+def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundaries_df_clr2_filename, resolution, binsize, window, rslt_df_name, change):
+
+    if change == 'intensity':
+        rslt_df = pd.read_csv(f'{rslt_df_name}', index_col=0)
+        df = rslt_df.sort_values('pvalue', key=abs, ascending=True).head(5)  # .dropna().tail(5)  # .head(5)
+        most_diff_tads = []
+        binsize = binsize * 1.5
+        for index, row in df.iterrows():
+            most_diff_tads.append([row['chrom'],  max(row['start_1'], row['start_2']) - binsize * 5,
+                                   max(row['end_1'], row['end_2']) + binsize * 5])
+        tads_annot = pd.DataFrame(most_diff_tads, columns=['Chromosome', 'Start', 'End'])
+        tads_annot['Start'] = tads_annot['Start'].apply(lambda x: x + 5 * binsize)
+        tads_annot['End'] = tads_annot['End'].apply(lambda x: x - 5 * binsize)
+
+    else:
+        rslt_df = pd.read_csv(f'{rslt_df_name}', sep='\t', index_col=0)
+
+        #rslt_df = rslt_df.drop_duplicates(subset=['chrom', 'start', 'end'], keep='first')
+
+        if change == 'merge':
+            tads_annot = rslt_df.iloc[[25, 100, 150, 200, 220]]
+        else:
+            tads_annot = rslt_df.iloc[[1, 110, 115, 210, 155]]
+
+        tads_annot = tads_annot.rename(columns={'chrom': 'Chromosome', 'start': 'Start', 'end': 'End'})
+        tads_annot['Start'] = tads_annot['Start'].apply(lambda x: x - binsize * 5)
+        tads_annot['End'] = tads_annot['End'].apply(lambda x: x + binsize * 5)
+        most_diff_tads = tads_annot.values.tolist()
+        tads_annot['Start'] = tads_annot['Start'].apply(lambda x: x + 5 * binsize)
+        tads_annot['End'] = tads_annot['End'].apply(lambda x: x - 5 * binsize)
+
 
     genes = pd.read_csv('ncbi_dataset.tsv',sep='\t')[['Chromosome', 'Begin', 'End', 'Gene_name', 'Symbol', 'Orientation']]
-    tads_annot = pd.DataFrame(most_diff_tads, columns=['Chromosome', 'Start', 'End'])
-    tads_annot['Start'] = tads_annot['Start'].apply(lambda x: x + 5 * binsize)
-    tads_annot['End'] = tads_annot['End'].apply(lambda x: x - 5 * binsize)
     tads_annot['Chromosome'] = tads_annot['Chromosome'].replace(REPLACE_DICT)
     genes = genes.rename(columns={'Begin': 'Start'})
     gr1, gr2 = pr.PyRanges(genes), pr.PyRanges(tads_annot)
     gr = gr1.intersect(gr2)
     tad_annots = gr.df
-    tad_annots.to_csv("tad_annots.csv")
-
     if not os.path.exists('graphics'):
         os.makedirs('graphics')
+    tad_annots.to_csv(f"graphics/annotation_{change}.csv")
 
     bp_formatter = EngFormatter('b')
     clr_1 = cooler.Cooler(f'{file_name_1}::resolutions/{resolution}')
@@ -65,15 +91,18 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             ax.xaxis.tick_bottom()
         if rotate:
             ax.tick_params(axis='x',rotation=45)
-
+    count_top = 0
     for i in most_diff_tads:
+        count_top+=1
         if i[0] == 'chrX':
             chrom = 'X'
         elif i[0] == 'chrY':
             chrom = 'Y'
         else:  # i[0] != 'chrX' or i[0] != 'chrY':
             chrom = re.findall("\d+", i[0])[0]
+
         tad_annot = tad_annots[tad_annots['Chromosome']==str(chrom)][tad_annots['Gene_name'].str.contains('uncharacterized|RNA|miR|pseudogene')==False][tad_annots['Symbol'].str.contains('LOC')==False] #[tdf['Orientation']=='plus']
+        tad_annot = tad_annot[tad_annot['Start'] >= i[1] - binsize][tad_annot['End'] <= i[2] + binsize]
 
         f, axs = plt.subplots(
             figsize=(12,4),
@@ -87,8 +116,11 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             vmax=800,
             extent=(start, end, end, start), cmap='gist_heat_r'
         );
-        clr1_tads = plot_tads(tads_annot[tads_annot['Chromosome']==str(chrom)], boundaries_df_clr1_filename)
-        tad_bgn = i[1]   
+
+
+        clr1_tads = plot_tads(tads_annot[tads_annot['Chromosome']==str(chrom)][tads_annot['Start'] >= i[1] - binsize][tads_annot['End'] <= i[2] + binsize],
+                              boundaries_df_clr1_filename, window)
+        tad_bgn = i[1]
         for index, row in clr1_tads.iterrows():
             tad_end = (row[3] + row[4])/2
             if tad_bgn == i[1]:
@@ -99,7 +131,8 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             tad_bgn = tad_end
         tad_end = i[2]
         ax.plot([tad_bgn, tad_end], [tad_bgn, tad_bgn], color='blue', linewidth=1, linestyle='-')
-        
+
+
         k=1
         for index, row in tad_annot.iterrows():
             if row['Orientation'] == 'minus':
@@ -108,8 +141,13 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             else:
                 bbox_props = dict(boxstyle="rarrow,pad=0.3", fc="palegreen", ec="g", lw=1)
                 ax.text(row['Start'], start - (k/1.6)* binsize, row['Symbol'], ha="center", va="top", rotation=0, size=5, bbox=bbox_props)
-        
-        ax.set_title(f'graphics/clr_1 {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+            k=2 if k==1 else 1
+
+        if change == 'intensity':
+            ax.set_title(f'{change} {count_top} {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+        else:
+            ax.set_title(f'{change} {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+
         plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04, label='raw counts');
         ax.set_ylabel('position, Mb')
         format_ticks(ax)
@@ -122,9 +160,11 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             vmax=800,
             extent=(start, end, end, start), cmap='gist_heat_r'
         );
-        
-        clr2_tads = plot_tads(tads_annot[tads_annot['Chromosome']==str(chrom)], boundaries_df_clr2_filename)
-        tad_bgn = i[1]   
+
+
+        clr2_tads = plot_tads(tads_annot[tads_annot['Chromosome']==str(chrom)][tads_annot['Start'] >= i[1] - binsize][tads_annot['End'] <= i[2] + binsize],
+                              boundaries_df_clr2_filename, window)
+        tad_bgn = i[1]
         for index, row in clr2_tads.iterrows():
             tad_end = (row[3] + row[4])/2
             if tad_bgn == i[1]:
@@ -135,30 +175,26 @@ def visualisation(file_name_1, file_name_2, boundaries_df_clr1_filename, boundar
             tad_bgn = tad_end
         tad_end = i[2]
         ax.plot([tad_bgn, tad_end], [tad_bgn, tad_bgn], color='blue', linewidth=1, linestyle='-')
-        
+
 
         k=1
         for index, row in tad_annot.iterrows():
             if row['Orientation'] == 'minus':
                 bbox_props = dict(boxstyle="larrow,pad=0.3", fc="palegreen", ec="g", lw=1)
-                ax.text(row['Start'], start - (k/1.6)* binsize, row['Symbol'], ha="center", va="top", rotation=0, size=5, bbox=bbox_props)
+                ax.text(row['Start'], start - (k/1.6)* binsize, row['Symbol'],
+                        ha="center", va="top", rotation=0, size=5, bbox=bbox_props)
             else:
                 bbox_props = dict(boxstyle="rarrow,pad=0.3", fc="palegreen", ec="g", lw=1)
-                ax.text(row['Start'], start - (k/1.6)* binsize, row['Symbol'], ha="center", va="top", rotation=0, size=5, bbox=bbox_props)
+                ax.text(row['Start'], start - (k/1.6)* binsize, row['Symbol'],
+                        ha="center", va="top", rotation=0, size=5, bbox=bbox_props)
+            k=2 if k==1 else 1
 
-        ax.set_title(f'clr_2 {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+        if change == 'intensity':
+            ax.set_title(f'{change} {count_top} {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+        else:
+            ax.set_title(f'{change} {i[0]}: {binsize * 5 +i[1]:,}-{i[2]- binsize * 5:,}', y=1.08)
+
         plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04, label='raw counts');
         format_ticks(ax)
-        plt.savefig(f"graphics/output_{i[0]}_{i[1]}_{i[2]}.jpg")
+        plt.savefig(f"graphics/{change}_{i[0]}_{i[1]}_{i[2]}.jpg")
         plt.tight_layout()
-
-
-clr1_filename = '4DNFIL6BHWZL_rs.mcool'
-clr2_filename = '4DNFIHXCPUAP_rs.mcool'
-resolution = 100_000
-window = 400_000
-binsize = 100_000
-boundaries_df_clr1_filename = '4DNFIL6BHWZL_rs.mcool_400000_boundaries.csv'
-boundaries_df_clr2_filename = '4DNFIHXCPUAP_rs.mcool_400000_boundaries.csv'
-rslt_df_name = 'pileup_df.csv'
-visualisation(clr1_filename, clr2_filename, boundaries_df_clr1_filename, boundaries_df_clr2_filename, resolution, binsize, rslt_df_name)
