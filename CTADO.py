bf33ff185f6d78d426c4c0ae77ed980b88e51609
Victoria Orlova <115810359+duckweedGuard@users.noreply.github.com>
2024-05-09 12:39:00 +0300

    Remove save option from CTADO.py

--- CTADO.py
+++ CTADO.py
@@ -28,7 +28,6 @@ if __name__ == "__main__":
     parser.add_argument('-r1', '--result_df_1_name', default=None, type=str, help='The first contact matrix dataframe name with chrom, start & end of TADs')
     parser.add_argument('-r2', '--result_df_2_name', default=None, type=str, help='The second contact matrix dataframe name with chrome, start & end of TADs')
     parser.add_argument('-df', '--result_dataframe_name', default=None, type=str, help='Dataframe name with intersecting TADs of two contact matrixes')
-    parser.add_argument('-s', '--save', type=bool, choices=(True, False), default=False, help='True if all result files should be saved, else False')
     parser.add_argument('-sd', '--save_directory', type=str, default='./', help='The path to the save directory')
     parser.add_argument('-lg', '--logging', type=bool, choices=(True, False), default=False, help='Enables logging')
     parser.add_argument('-t', '--threads', type=int, default=1, help='Parameter for specifying the number of threads')
@@ -43,7 +42,7 @@ if __name__ == "__main__":
 
     count_tads_change_intensity(args.clr1_filename, args.clr2_filename, args.resolution, args.window,
                                 args.flank, args.binsize, args.clr1_boundaries_name, args.clr2_boundaries_name,
-                                args.result_df_1_name, args.result_df_2_name, args.result_dataframe_name, args.save,
+                                args.result_df_1_name, args.result_df_2_name, args.result_dataframe_name,
                                 args.save_directory, args.threads)
     main_split_merge_detection(args.clr1_filename, args.clr2_filename, args.resolution, args.binsize,
                                f'{args.save_directory}/{args.clr1_filename}_{args.window}_result_df.csv',
