af0c12cf39756ff4cb34c55eaa6aa777b7bd3c4a
Ivan Kozin <brayin.2016@gmail.com>
2024-05-10 15:48:53 +0300

    Correct margins, add information about default values

--- CTADO.py
+++ CTADO.py
@@ -6,6 +6,7 @@ from src.calculate_intensity_change import count_tads_change_intensity
 from src.tads_plot import visualisation
 from src.split_merge_detect import main_split_merge_detection
 
+
 INTENSITY = 'intensity_change_result.csv'
 SPLIT = 'split_coords.csv'
 MERGE = 'merge_coords.csv'
@@ -13,26 +14,41 @@ MERGE = 'merge_coords.csv'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                      prog='Differential analysis of interacting domains between two contact matrixes', 
-                      description='This tool is needed to find four types of changes in TADs between two contact matrixes', 
+                      prog='Differential analysis of interacting domains between two contact matrices',
+                      description='This tool is needed to find four types of changes in TADs\
+                       between two contact matrices',
+                      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                       epilog='Good luck!   (∿°○°)∿ .・。.・゜✭・.・。.・゜✭・.・。.・゜✭')
 
-    parser.add_argument('clr1_filename', type=str, help='Name of first contact matrix in mcool/cool format')
-    parser.add_argument('clr2_filename', type=str, help='Name of second contact matrix in mcool/cool format')
+    parser.add_argument('clr1_filename', type=str,
+                        help='Name of first contact matrix in mcool/cool format')
+    parser.add_argument('clr2_filename', type=str,
+                        help='Name of second contact matrix in mcool/cool format')
     parser.add_argument('resolution', type=int, help='Resolution of mcool file')
     parser.add_argument('window', type=int, help='Size of the sliding diamond window')
     parser.add_argument('flank', type=int, help='Flank size in bp')
     parser.add_argument('binsize', type=int, help='Bin size in bp')
-    parser.add_argument('clr1_boundaries_name', type=str, help='The first contact matrix boundaries argument, a dataframe name with TADs boundaries in chrom, start, end format or cooler insulation table')
-    parser.add_argument('clr2_boundaries_name', type=str, help='The second contact matrix boundaries argument, a dataframe name with TADs boundaries in chrom, start, end format or cooler insulation table')
-    parser.add_argument('-r1', '--result_df_1_name', default=None, type=str, help='The first contact matrix dataframe name with chrom, start & end of TADs')
-    parser.add_argument('-r2', '--result_df_2_name', default=None, type=str, help='The second contact matrix dataframe name with chrome, start & end of TADs')
-    parser.add_argument('-df', '--result_dataframe_name', default=None, type=str, help='Dataframe name with intersecting TADs of two contact matrixes')
-    parser.add_argument('-sd', '--save_directory', type=str, default='./', help='The path to the save directory')
-    parser.add_argument('-lg', '--logging', type=bool, choices=(True, False), default=False, help='Enables logging')
-    parser.add_argument('-t', '--threads', type=int, default=1, help='Parameter for specifying the number of threads')
+    parser.add_argument('clr1_boundaries_name', type=str,
+                        help='The first contact matrix boundaries argument,\
+                         a dataframe name with TADs boundaries in chrom, start, end format or cooler insulation table')
+    parser.add_argument('clr2_boundaries_name', type=str,
+                        help='The second contact matrix boundaries argument,\
+                         a dataframe name with TADs boundaries in chrom, start, end format or cooler insulation table')
+    parser.add_argument('-r1', '--result_df_1_name',
+                        default=None, type=str, help='The first contact matrix dataframe name with chrom,\
+                         start & end of TADs')
+    parser.add_argument('-r2', '--result_df_2_name',
+                        default=None, type=str, help='The second contact matrix dataframe name with chrome,\
+                         start & end of TADs')
+    parser.add_argument('-df', '--result_dataframe_name',
+                        default=None, type=str, help='Dataframe name with intersecting TADs of two contact matrices')
+    parser.add_argument('-od', '--output_directory', type=str,
+                        default='./', help='The path to the save directory')
+    parser.add_argument('-lg', '--logging', type=bool, choices=(True, False),
+                        default=False, help='Enables logging')
+    parser.add_argument('-t', '--threads', type=int,
+                        default=1, help='Parameter for specifying the number of threads')
     args = parser.parse_args()
-
 
     logger = logging.getLogger()
     logger.disabled = not args.logging
@@ -48,8 +64,12 @@ if __name__ == "__main__":
                                f'{args.save_directory}/{args.clr1_filename}_{args.window}_result_df.csv',
                                f'{args.save_directory}/{args.clr2_filename}_{args.window}_result_df.csv',
                                args.save_directory)
-    sys.stdout.write(f'Visualising...\n'); sys.stdout.flush()
+    sys.stdout.write(f'Visualising...\n')
+    sys.stdout.flush()
     for file in [INTENSITY, SPLIT, MERGE]:
         type_of_change = file[:file.find('_')]
-        visualisation(args.clr1_filename, args.clr2_filename, args.clr1_boundaries_name, args.clr2_boundaries_name, args.resolution, args.binsize, args.window, f'{args.save_directory}/{file}', type_of_change, args.save_directory)
-    sys.stdout.write(f'CTADO completed successfully! Output location:\n{os.path.abspath(args.save_directory)}\n'); sys.stdout.flush()
+        visualisation(args.clr1_filename, args.clr2_filename, args.clr1_boundaries_name, args.clr2_boundaries_name,
+                      args.resolution, args.binsize, args.window, f'{args.save_directory}/{file}',
+                      type_of_change, args.save_directory)
+    sys.stdout.write(f'CTADO completed successfully! Output location:\n{os.path.abspath(args.save_directory)}\n')
+    sys.stdout.flush()
