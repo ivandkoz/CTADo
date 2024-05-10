f52c6217ebb8767b86196e51170eaea0470f625b
Ivan Kozin <brayin.2016@gmail.com>
2024-05-10 15:26:28 +0300

    Add type annotation and optimize import

--- src/func_condition_wrapper.py
+++ src/func_condition_wrapper.py
@@ -1,17 +1,22 @@
 import sys
 import traceback
+import typing
 
 FUNC_NAMES = {'count_tads_change_intensity': ['Searching changes in the intensity of TADs...',
                                               'The number of TADs with a changed intensity: '],
               'main_split_merge_detection': ['Searching splits and merges in TADs...',
                                              'The number of splits|merges: ']
               }
-def wrapper_print(func):
-    def wrapper(*args, **kwargs):
-        sys.stdout.write(f'{FUNC_NAMES[func.__name__][0]}\n'); sys.stdout.flush()
+
+
+def wrapper_print(func: typing.Callable) -> typing.Callable:
+    def wrapper(*args, **kwargs) -> typing.NoReturn:
+        sys.stdout.write(f'{FUNC_NAMES[func.__name__][0]}\n')
+        sys.stdout.flush()
         try:
             result = func(*args, **kwargs)
-            sys.stdout.write(f'Completed successfully! {FUNC_NAMES[func.__name__][1]}{result}\n'); sys.stdout.flush()
+            sys.stdout.write(f'Completed successfully! {FUNC_NAMES[func.__name__][1]}{result}\n')
+            sys.stdout.flush()
         except Exception as e:
             sys.stderr.write(traceback.format_exc())
 
